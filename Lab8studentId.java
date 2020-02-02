0b7b398f2a47af958f4915f8532ea34ed845e7ac
Rana-Siroosian <59078216+Rana-Siroosian@users.noreply.github.com>
2020-02-02 18:01:24 -0500

    Update Lab8studentId.java

--- Lab8studentId.java
+++ Lab8studentId.java
@@ -1,152 +1,131 @@
-import java.io.IOException;
 import java.util.Scanner;
 /**
+ * 
  * 
  * @author >>RanaSiroosian<<
  *
  */
-public class Lab8studentId {
+public class Lab8Student {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 		
 		Scanner scnr = new Scanner (System.in);
 		
-		int stuId = 0;
+		int studentId=0;
+		System.out.print("Welcome to our Java class. ");
+		boolean flag=false;
+		boolean looping = true;
+		boolean flag1 = false;
+		String question="";
+		boolean loop = false;
 		
-		System.out.println("Welcome to our Java class. "
-				+ "Which student would you like to learn more about? (enter  a number 1-10):  ");
-		
-		stuId= scnr.nextInt();
-		
-		String firstName = studentFirstName(stuId);
-		String lastName = studentLastName(stuId);
-		
-		System.out.println("student " +stuId + " is "+ firstName + " " + lastName + ". what would you like to "
-				+"know about "+ firstName +
-				"? (enter or “Favorite food” or “Age” or “Hometown” or “Hobby”):");
-		
-		scnr.nextLine();
-		String info = scnr.nextLine();
-		userChoices(info, stuId);
-		
-		scnr.close();
-	}
-	public static void userChoices(String choice, int id) {
-		
-		Scanner scnr = new Scanner (System.in);
-		
-		String[] firstName = {"Rana ","Fernanda ", "Jennifer ", "Sarah ", 
-				 "Enrique ","Shahin ", "Pete ", "Mina ",
-				"Brad "};
-		int [] studentAge = {30, 24, 36, 26, 38, 44, 44, 13,64};
-		
-		String yesOrNo = "";
-		boolean valid = true;
-		
-		while (valid) {
+		do {
 			
-			if (choice.equalsIgnoreCase("Favorite food")) 
-			
-				System.out.print(studentFirstName(id) +"'s favorite food is " + favoriteFood(id)+".");
+			System.out.println("Which student would you like to learn more about? (enter a number 1-10): ");
+			studentId = scnr.nextInt();
 		
-			else if (choice.equalsIgnoreCase("Age")) 
+			do {
+				try {
 			
-				System.out.print(firstName + "is " + studentAge[id-1] +".");
+					if (studentId>0 && studentId<11) {
+						loop=true;
+						System.out.println("Student " + studentId + " is " + studentName(studentId-1) + ". Would you like to know more? (enter \"yes\" or \"no\")");
+						scnr.nextLine();
+						question = scnr.nextLine();
+						break;
+					}
+					throw new IndexOutOfBoundsException();
+				}catch(Exception IndexOutOfBoundsException) {
+					
+				System.err.println("Please try again! (enter a number 1-10): ");
+				studentId=scnr.nextInt();
+				}
+			}while(!loop);
 		
-			else if (choice.equalsIgnoreCase("Hometown")) 
-			
-				System.out.print(studentFirstName(id) + "'s hometown is " + studentHomeTown(id) + ".");
+			do {
 		
-			else if (choice.equalsIgnoreCase("Hobby")) 
-			
-				System.out.print(studentFirstName(id) + "'s hobby is " + studentHobby(id) + ".");
-		
-			else {
+				if (question.equalsIgnoreCase("yes")) {
+					
+					flag = true;
+					System.out.println("What would you like to know about "+ studentName(studentId-1)+"? "
+							+ "(enter \"Favorite food\" or \"Favorite country\" or \"Age\")");
+					
+					question = scnr.nextLine();
+					
+					do {
+						
+						try {
+							if(question.equalsIgnoreCase("Favorite food")) {
+								flag1 = true;
+								favoriteFood(studentId-1);
+								break;
+							}
+							if(question.equalsIgnoreCase("favorite country")){
+								flag1 = true;
+								favoriteCountry(studentId-1);
+								break;
+							}
+							if(question.equalsIgnoreCase("Age")) {
+								flag1 = true;
+								age(studentId-1);
+								break;
+							}
+							throw new IllegalArgumentException();
+						}catch(Exception IllegalArgumentException) {
+						
+							System.err.println("Please try again! (enter\"Favorite food\" or \"Favorite country\" or \"Age\")");
+							question = scnr.nextLine();
+						}
+					}while(!flag1);
+					
+					System.out.println(" Would you like to know more? (enter \"yes\" or \"no\")");
+					question= scnr.nextLine();
+					if(question.equalsIgnoreCase("no")) {
+						flag = false;
+					} 
 				
-				System.out.println("That data does not exist. "
-						+ "Please try again. (enter or “Favorite food” or “Age” or"
-						+ "'s “Hometown” or “Hobby”):");
-				
-				choice = scnr.nextLine();
-				userChoices(choice,id);
-				
+				}
+			}while (flag);
+			System.out.println("Would you like to know about another student? "
+					+ "(enter \"yes\" or \"no\")");
+			question=scnr.nextLine();
+			if(question.equalsIgnoreCase("no")) {
+				looping = false;
 			}
-			try {
-			System.out.println(" Would you like to know more? (enter “yes” or “no“): ");
-			yesOrNo = scnr.nextLine();
-			
-			if (yesOrNo.equalsIgnoreCase("yes")) {
-				
-				System.out.println("What would you like to know about " + studentFirstName(id) + 
-						"? (enter or “Favorite food” or “Age” or “Hometown” or “Hobby”):");
-				
-			choice = scnr.nextLine();
-			userChoices(choice,id);
-			
-		}}catch (Exception e) {
-			
-		valid = false;
-		System.out.println("Thanks!");
-
-		}
-		}
-//		System.out.println("Thanks!");
-
+		}while (looping);
+		
+		System.out.println("Thank you, bye.");
 		scnr.close();
 	}
-			
-	public static String studentFirstName(int id) {
+	
+	public static String studentName(int num) {
+		String [] name = {"Rana Siroosian", "John Clone", "Sara Green", "Mary French",
+				"Pam Brown", "Sandy Ellen", "James Crowd", "Bill Dobald", "Drake Bryan", "Mike Anderson"};
 		
-		Scanner scan = new Scanner (System.in);
-//		String name="";
-//		boolean valid1 = true;
-		String[] firstName = {"Rana ","Fernanda ", "Jennifer ", "Sarah ", 
-				 "Enrique ","Shahin ", "Pete ", "Mina ",
-				"Brad "};
-		
-		if (id>10 || id <0) {
-			System.out.println("doesn't exist. please enter a number between 1-10: ");
-//			scan.next();
-			id = scan.nextInt();
-//			studentFirstName(id);
-//			name = firstName[id-1];
-		}
-		return firstName[id-1];
-
+		return name[num];
 	}
 	
-	public static String studentLastName(int id) {
+	public static void favoriteFood (int stf) {
+		String[] food = {"Sushi", "Pizza", "Spicy Chicken", "Tuna Salad", "Hamburger", "Mac & Cheese",
+				"BBQ Chicken", "Smoked Salmon", "Steak","Chicken Sandwich"};
 		
-		String[] lastName = {"Siroosian","Franco", "Brown", "Green","Iglesias","Bouzari",
-				"McGarty","Akbari","Pitt"};
-		
-		return lastName[id-1];
+		System.out.print(studentName(stf) + "' favorite food is " + food[stf]+".");
 	}
 	
-	public static String favoriteFood(int x) {
-		
-	
-		String[] favFood = {"Sushi", "Taco", "Pizza", "Mac&Cheese", "Pasta","Hamburger", "Chicken Alfredo",
-				"Steak","BBQ chicken"};
-		
-		return favFood[x-1];
-	}
-	
-
-	public static String studentHomeTown(int home) {
-		
-		String [] homeTown = {"Westland,MI", "Taylor,MI", "Romulus,MI","Dearborn,MI","Westland,MI",
-				"Livonia,MI", "Ann Arbor,MI","Plymouth,MI", "Westland,MI"};
-		
-		return homeTown[home-1];
-	}
-	
-	public static String studentHobby(int hobby) {
-		
-		String [] stuHobby = {"Drawing", "Traveling", "Writing", "Dancing", "Shopping", "Reading books",
-				"Watching NBA games", "Playing video games","Cooking"};
-		
-		return stuHobby[hobby-1];
+	public static void favoriteCountry(int stc) {
+		String[] country = {"United States", "France", "Russia", "Brazil", "Paris", "Italy","Canada",
+				"mexico","Spain","China"};
+		System.out.print(studentName(stc) + "' favorite country is " + country[stc] + ".");
 	}
 
+	public static void age(int sta) {
+		int[] ages = {30 , 32, 28, 24, 31, 29, 36, 30, 37, 33};
+		
+		System.out.print(studentName(sta) + " is " + ages[sta] + ".");
+	}
+	
 }
+	
+
+
+
